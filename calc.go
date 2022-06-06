5776749835c4c67a974935dc688f5e48aeabddc8
iobk <iobkyb@gmail.com>
2022-06-06 23:56:58 +0900

    update v1.o

--- calc.go
+++ calc.go
@@ -1,10 +1,12 @@
 package calc
 
+import "fmt"
+
 func Add(a, b int) int {
-	println("calc package Add() function v1.0")
+	fmt.Println("calc package Add() function v1.0")
 	return a + b
 }
 func Add1(a, b int) int {
-	println("calc package Add1() function v1.o")
+	fmt.Println("calc package Add1() function v1.o")
 	return a + b
 }
