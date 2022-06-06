13afbdce9a5943818398ecd0bb0fdc8d367ee141
iobk <iobkyb@gmail.com>
2022-06-07 00:43:51 +0900

    tag 1.0.0

--- calc.go
+++ calc.go
@@ -3,10 +3,10 @@ package calc
 import "fmt"
 
 func Add(a, b int) int {
-	fmt.Println("calc package Add() function v1.1")
+	fmt.Println("calc package Add() function v1.0.0")
 	return a + b
 }
 func Add1(a, b int) int {
-	fmt.Println("calc package Add1() function v1.1")
+	fmt.Println("calc package Add1() function v1.0.0")
 	return a + b
 }
