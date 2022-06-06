e5f303bb4ccc112252539e0b0c7b4ed9bf81e42a
iobk <iobkyb@gmail.com>
2022-06-06 23:51:57 +0900

    v1.0

--- calc.go
+++ calc.go
@@ -1,10 +1,10 @@
 package calc
 
 func Add(a, b int) int {
-	println("calc package Add() function")
+	println("calc package Add() function v1.0")
 	return a + b
 }
 func Add1(a, b int) int {
-	println("calc package Add1() function")
+	println("calc package Add1() function v1.o")
 	return a + b
 }
